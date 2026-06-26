8184b4cca736ffd51ba28c656e4def0638b34299
Zestros <dardanel2006dan@mail.ru>
2026-06-26 21:55:42 +0800

    Create structure project

---
+++ src/course_connector/__init__.py
@@ -0,0 +1,1 @@
+"""Course connector package."""
---
+++ src/course_connector/cli.py
@@ -0,0 +1,10 @@
+"""Command line entry point for course connector."""
+
+
+def main() -> None:
+    """Run the course connector CLI."""
+    print("course_connector CLI")
+
+
+if __name__ == "__main__":
+    main()
